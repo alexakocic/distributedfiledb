4411736714c638d8885184913d367d26e427ed33
Aleksa Kocic <aleksa.kocic@vulpes.rs>
2020-02-05 21:38:21 +0100

    Added initial projects to solution

---
+++ Interface/DistributedFileDB.Interface.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DistributedFileDB.Interface.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Model/DistributedFileDB.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DistributedFileDB.Model
+{
+    public class Class1
+    {
+    }
+}
